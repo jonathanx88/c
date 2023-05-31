280db7eca14f9b5d55828b77f0a4f3026cea61b1
jonathanx88 <128701201+jonathanx88@users.noreply.github.com>
2023-05-31 11:18:16 -0300

    Add files via upload

---
+++ atvd.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+int i,par,impar=0;
+int num[6];
+for  i = 0; i < 6; i++);
+{
+    printf("digite um numero %d :",i+);
+    scanf("%d",&num[i]);
+
+    if (num[i] % ==0)
+    {
+        par++;
+    }
+    else
+   impar++;
+
+for ( i = 0; i < 6; i++)
+{
+
+}
+
+}
+
+  printf("numero %d:%0.f \n" ,num[i]);
+printf("numeros pares%d,\n",num[i]);
+printf("numeros imapres%d,\n",num[i]);
+
+
+
+return 0
+}
